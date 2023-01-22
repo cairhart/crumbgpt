b201db4507ef96ee91240310062b1da9a98170d9
Carter Airhart <airhartcarter@utexas.edu>
2023-01-21 20:15:30 -0800

    01-21-2023

--- train.py
+++ train.py
@@ -1,6 +1,20 @@
 import os
+import tiktoken
 
 with open('input.txt', 'r', encoding='utf-8') as f:
 	text = f.read()
 
 print("char length", len(text))
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+
+enc = tiktoken.get_encoding('gpt2')
+
+encode = lambda s: enc.encode(s)
+decode = lambda l: enc.decode(l)
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
